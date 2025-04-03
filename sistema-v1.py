c54d5dab56348ec14c7bb5df93b750b4cb1648c9
Guilherme Fontebasse <fontebasse@icloud.com>
2025-04-03 14:07:45 -0300

    primeira versao finalizada

--- sistema-v1.py
+++ sistema-v1.py
@@ -1,3 +1,4 @@
+
 menu = """
            ========== MENU ==========
 
@@ -10,33 +11,45 @@ menu = """
            [4] Sair
 
            ========================="""
-
-print(menu)
 saldo = 1000
-limite = 500
+LIMITE = 500.00
 extrato = []
 numero_saques = 1
-limite_saques = 3
+LIMITE_DE_SAQUES = 3
 
+print(menu)
 
 while True:
     opcao = int(input("Informe a opção desejada: "))
     if opcao == 1:
         valor_de_saque = float(input("Informe quanto deseja sacar: "))
-        if valor_de_saque <= saldo and numero_saques <= limite_saques:
+        if valor_de_saque > saldo or valor_de_saque > LIMITE:
+            print("Valor insuficiente :(")
+
+        elif valor_de_saque <= saldo and numero_saques <= LIMITE_DE_SAQUES:
             print("operação realizada")
             numero_saques = numero_saques + 1
             saldo = saldo - valor_de_saque
-            extrato.append(f"Saque realizado, saldo atual:R$:{saldo}")
-            print(f"Seu saldo atual é de:R${saldo}")
-            print("\n".join(extrato))
-        elif valor_de_saque > saldo and valor_de_saque > limite:
-            print("Valor insuficiente :(")
+            extrato.append(
+                f"Saque R${valor_de_saque:.2f}. Saldo R${saldo:.2f}")
+            print(f"Seu saldo atual: R${saldo:.2f}")
 
-        elif numero_saques >= limite_saques:
-            print("limite de saque atingido, tente novamente amanhã")
+        elif numero_saques >= LIMITE_DE_SAQUES:
+            print("limite de saque diario, tente novamente amanhã")
+
+    elif opcao == 2:
+        deposito = float(input("Informe quanto deseja depositar: "))
+        saldo = deposito + saldo
+        extrato.append(f"Deposito R${deposito:.2f}. Saldo R${saldo:.2f}")
+        print(f"seu saldo atual: R${saldo:.2f} ")
+
     elif opcao == 3:
         print("\n".join(extrato))
-        print(f"saldo atual:R$:{saldo}")
+        print(f"saldo atual R${saldo:.2f}")
+
     elif opcao == 4:
         break
+
+    else:
+        print("Opção invalida")
+        continue
