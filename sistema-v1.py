354560115f8b0c485741e0feef80a4ccaa8ba1c8
Guilherme Fontebasse <fontebasse@icloud.com>
2025-04-02 23:24:46 -0300

    add extrato

--- sistema-v1.py
+++ sistema-v1.py
@@ -1,37 +1,42 @@
 menu = """
-           ========== MENU ========== 
-    
-    Bem-vindo ao sistema de gerenciamento financeiro!
-           O que você deseja fazer?          
+           ========== MENU ==========
 
-           [1] Sacar 
-           [2] Depositar 
-           [3] Extrato 
+    Bem-vindo ao sistema de gerenciamento financeiro!
+           O que você deseja fazer?
+
+           [1] Sacar
+           [2] Depositar
+           [3] Extrato
            [4] Sair
-           
+
            ========================="""
 
 print(menu)
-saldo = 10000
+saldo = 1000
 limite = 500
 extrato = []
-numero_saques = 0
+numero_saques = 1
 limite_saques = 3
 
 
-opcao = int(input("Informe a opção desejada: "))
-
-while opcao == 1:
-
+while True:
+    opcao = int(input("Informe a opção desejada: "))
     if opcao == 1:
         valor_de_saque = float(input("Informe quanto deseja sacar: "))
-        if valor_de_saque > saldo or valor_de_saque > limite:
+        if valor_de_saque <= saldo and numero_saques <= limite_saques:
+            print("operação realizada")
+            numero_saques = numero_saques + 1
+            saldo = saldo - valor_de_saque
+            extrato.append(f"Saque realizado, saldo atual:R$:{saldo}")
+            print(f"Seu saldo atual é de:R${saldo}")
+            print("\n".join(extrato))
+        elif valor_de_saque > saldo and valor_de_saque > limite:
             print("Valor insuficiente :(")
 
-        elif valor_de_saque <= saldo and numero_saques <= limite_saques:
-            print("operação realizada")
-            numero_saques = +1
-            saldo = valor_de_saque - saldo
-            extrato.append(f"Saque realizado, saldo atual: {saldo}")
-        if True:
-            break
+        elif numero_saques >= limite_saques:
+            print("limite de saque atingido, tente novamente amanhã")
+    elif opcao == 3:
+        print("\n".join(extrato))
+        print(f"saldo atual:R$:{saldo}")
+    elif opcao == 4:
+        break
